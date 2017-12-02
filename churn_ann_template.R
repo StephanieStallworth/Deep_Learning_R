0b4874b3e2f16db94da6ba072a3eb2fbb6e4e8a7
Stephanie Stallworth <scstallworth1@gmail.com>
2017-12-02 18:26:43 -0500

    Add files via upload

---
+++ churn_ann_template.R
@@ -0,0 +1,32 @@
+# Artificial Neural Network ANN
+
+# Data Preprocessing Template
+
+# Importing the dataset
+dataset = read.csv('Churn_Modelling.csv')
+dataset = dataset[ ,4:14]
+
+# Encoding categorical variables as factors
+dataset$Geography = as.numeric(factor(dataset$Geography,
+                              levels = c('France', 'Spain', 'Germany'),
+                              labels = c(1, 2, 3)))
+dataset$Gender = as.numeric(factor(dataset$Gender,
+                           levels = c('Female', 'Male'),
+                           labels = c(1, 2)))
+
+
+# Splitting the dataset into the Training set and Test set
+# install.packages('caTools')
+library(caTools)
+set.seed(123)
+split = sample.split(dataset$Exited, SplitRatio = 0.8)
+training_set = subset(dataset, split == TRUE)
+test_set = subset(dataset, split == FALSE)
+
+# Feature Scaling
+training_set = scale(training_set[-11])
+test_set = scale(test_set[-11])
+
+# Fitting ANN to the Training set
+install.packages('h20')
+library(h20)
